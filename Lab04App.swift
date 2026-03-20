091cffc8c47849ea9a956e84f2662eaf14302220
liamm78 <liam.murphy3774@gmail.com>
2026-03-20 15:00:47 -0400

    first commit

---
+++ Lab04App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab04App.swift
+//  Lab04
+//
+//  Created by lending on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
